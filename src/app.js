925c435f94ca80c4a4b066370720b88e9c95af58
Vincent Iroleh <irolehiroleh@gmail.com>
2021-03-20 00:36:45 +0100

    omo, creating content is not easy

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Import modules
 const express = require("express");
 const path = require("path");
@@ -5,17 +7,34 @@ const path = require("path");
 // Instantiating express
 const app = express();
 
-// Setup EJS engine and make public directory accessible
+// App settings
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// App middleware
 app.use(express.static("public"));
 
-// Serve our ejs file on the / endpoint
+// App routes
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Handel unmatched route
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: {
+      name: "Error",
+      status: 404,
+      message: "Invalid Request",
+      statusCode: 404,
+      redirect: "http://localhost:8080/",
+    },
+  });
+  next();
+});
+
+// App Server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`App listening on http://127.0.0.1:${port}`);
+  console.log(`App listening on http://localhost:${port}`);
 });
